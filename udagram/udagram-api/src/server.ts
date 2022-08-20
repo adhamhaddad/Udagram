9d776da6cdb95b3d6b09b5ac57d4aa90626f9892
adhamhaddad <adhamhaddad.dev@gmail.com>
2022-08-20 20:22:43 +0200

    new changes

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -2,9 +2,7 @@ import * as dotenv from "dotenv";
 import cors from 'cors';
 import express from "express";
 import { sequelize } from "./sequelize";
-
 import { IndexRouter } from "./controllers/v0/index.router";
-
 import bodyParser from "body-parser";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
