14d6972626a924011c7aeda6ab10d84d2aa1f6d1
adhamhaddad <adhamhaddad.dev@gmail.com>
2022-08-20 20:12:40 +0200

    new changes

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagramapi-env.eba-wvjxq24w.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-env.eba-xxdrpvrv.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
